ecd2302dd510e099ed66b456b6a2c5794f6390b1
Shambler34 <sci.en.tist@hotmail.com>
2023-04-04 14:16:31 +0200

    real init commit

---
+++ ts_src/ts_xapp/CMakeFiles/ts_xapp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ts_xapp.dir/ts_xapp.cpp.o"
+  "ts_xapp"
+  "ts_xapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ts_xapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ts_src/utils/CMakeFiles/utils_objects.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utils_objects.dir/restclient.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/utils_objects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
